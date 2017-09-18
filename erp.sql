903b671f616de1435e505926aa462c70e7315c96
frdev <felipertw2@gmail.com>
2017-09-17 21:10:04 -0300

    Base atualizada

--- erp.sql
+++ erp.sql
@@ -1,64 +1,55 @@
--- phpMyAdmin SQL Dump
--- version 4.7.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: 04-Set-2017 às 17:05
--- Versão do servidor: 10.1.24-MariaDB
--- PHP Version: 7.1.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `erp`
---
-
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           10.1.26-MariaDB - mariadb.org binary distribution
+-- OS do Servidor:               Win32
+-- HeidiSQL Versão:              9.4.0.5125
 -- --------------------------------------------------------
 
---
--- Estrutura da tabela `compras`
---
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-CREATE TABLE `compras` (
-  `id` int(11) NOT NULL,
+
+-- Copiando estrutura do banco de dados para erp
+CREATE DATABASE IF NOT EXISTS `erp` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `erp`;
+
+-- Copiando estrutura para tabela erp.compras
+CREATE TABLE IF NOT EXISTS `compras` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_produto` int(11) NOT NULL,
   `id_fornecedor` int(11) NOT NULL,
   `id_usuario` int(11) NOT NULL,
   `qtd` int(11) NOT NULL,
   `valor` varchar(255) NOT NULL,
   `status` int(11) NOT NULL DEFAULT '1',
+  `nfcompra` varchar(10) DEFAULT NULL,
   `datasolicitacao` date NOT NULL,
   `dataaprovado` date DEFAULT NULL,
-  `dataentregue` date DEFAULT NULL
+  `dataentregue` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_produto` (`id_produto`),
+  KEY `fk_usuario` (`id_usuario`),
+  KEY `fk_fornecedor` (`id_fornecedor`),
+  CONSTRAINT `fk_fornecedor` FOREIGN KEY (`id_fornecedor`) REFERENCES `fornecedores` (`id`),
+  CONSTRAINT `fk_produto` FOREIGN KEY (`id_produto`) REFERENCES `produtos` (`id`),
+  CONSTRAINT `fk_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+-- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela erp.departamentos
+CREATE TABLE IF NOT EXISTS `departamentos` (
+  `id` varchar(3) DEFAULT NULL,
+  `descricao` varchar(50) DEFAULT NULL,
+  `status` tinyint(4) DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Extraindo dados da tabela `compras`
---
-
-INSERT INTO `compras` (`id`, `id_produto`, `id_fornecedor`, `id_usuario`, `qtd`, `valor`, `status`, `datasolicitacao`, `dataaprovado`, `dataentregue`) VALUES
-(1, 2, 2, 1, 50, '900.00', 4, '2017-09-03', '2017-09-04', '2017-09-04'),
-(2, 3, 2, 2, 16, '200.00', 4, '2017-09-04', '2017-09-04', '2017-09-04'),
-(3, 3, 3, 1, 15, '300', 0, '2017-09-04', NULL, NULL),
-(4, 3, 2, 1, 2, '200', 4, '2017-09-04', '2017-09-04', '2017-09-04');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `fornecedores`
---
-
-CREATE TABLE `fornecedores` (
-  `id` int(11) NOT NULL,
+-- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela erp.fornecedores
+CREATE TABLE IF NOT EXISTS `fornecedores` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `cnpj` varchar(20) NOT NULL,
   `razaosocial` varchar(100) NOT NULL,
   `fantasia` varchar(100) NOT NULL,
@@ -71,47 +62,44 @@ CREATE TABLE `fornecedores` (
   `cidade` varchar(50) NOT NULL,
   `uf` varchar(2) NOT NULL,
   `cep` varchar(8) NOT NULL,
-  `status` varchar(20) NOT NULL DEFAULT 'Ativo'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `status` varchar(20) NOT NULL DEFAULT 'Ativo',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `razaosocial` (`razaosocial`),
+  UNIQUE KEY `fantasia` (`fantasia`),
+  UNIQUE KEY `cnpj` (`cnpj`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
---
--- Extraindo dados da tabela `fornecedores`
---
-
-INSERT INTO `fornecedores` (`id`, `cnpj`, `razaosocial`, `fantasia`, `responsavel`, `telefone`, `email`, `logradouro`, `numero`, `complemento`, `cidade`, `uf`, `cep`, `status`) VALUES
-(2, '12345678912342', 'FELIPE RISTOW DEVELOPER', 'FRDEV', 'FELIPE RISTOW', '11971486451', 'felipertw2@gmail.com', 'Rua Ipanema', '686', 'APTO 112B', 'São Paulo', 'SP', '03164200', 'Inativo'),
-(3, '012481547912345', 'CLINICA PETS MADU', 'MADU PETS', 'MARIA EDUARDA', '11949069047', 'mariaeduardaes@live.com', 'Rua Ipanema', '686', 'APTO 112B', 'São Paulo', 'SP', '03164200', 'Ativo');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `historico`
---
-
-CREATE TABLE `historico` (
-  `id` int(11) NOT NULL,
+-- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela erp.historico
+CREATE TABLE IF NOT EXISTS `historico` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_produto` int(11) NOT NULL,
   `movimentacao` varchar(20) NOT NULL,
   `qtd` int(11) NOT NULL,
-  `data` date NOT NULL
+  `data` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_produto_historico` (`id_produto`),
+  CONSTRAINT `fk_produto_historico` FOREIGN KEY (`id_produto`) REFERENCES `produtos` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+-- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela erp.producoes
+CREATE TABLE IF NOT EXISTS `producoes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_produto` int(11) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `datasolicitacao` date DEFAULT NULL,
+  `dataaprovacao` date DEFAULT NULL,
+  `dataentrega` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_produto_producao` (`id_produto`),
+  CONSTRAINT `fk_produto_producao` FOREIGN KEY (`id_produto`) REFERENCES `produtos` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Extraindo dados da tabela `historico`
---
-
-INSERT INTO `historico` (`id`, `id_produto`, `movimentacao`, `qtd`, `data`) VALUES
-(1, 1, 'Venda', 2, '2017-09-04'),
-(2, 3, 'Compra', 2, '2017-09-04');
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `produtos`
---
-
-CREATE TABLE `produtos` (
-  `id` int(11) NOT NULL,
+-- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela erp.produtos
+CREATE TABLE IF NOT EXISTS `produtos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `descricao` varchar(255) NOT NULL,
   `tipo` varchar(255) NOT NULL,
   `status` tinyint(1) NOT NULL,
@@ -119,38 +107,29 @@ CREATE TABLE `produtos` (
   `qtd` int(11) NOT NULL,
   `acionamento` int(11) NOT NULL,
   `min` int(11) NOT NULL,
-  `max` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `max` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
---
--- Extraindo dados da tabela `produtos`
---
-
-INSERT INTO `produtos` (`id`, `descricao`, `tipo`, `status`, `preco`, `qtd`, `acionamento`, `min`, `max`) VALUES
-(1, 'teste', '0', 1, 50, 4, 9, 3, 15),
-(2, 'teste1', '1', 1, 20, 114, 15, 10, 30),
-(3, 'teste2', '2', 1, 30, 16, 15, 12, 30);
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `reposicoes`
---
-
-CREATE TABLE `reposicoes` (
-  `id` int(11) NOT NULL,
+-- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela erp.reposicoes
+CREATE TABLE IF NOT EXISTS `reposicoes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_produto` int(11) NOT NULL,
-  `qtd` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id_departamento` varchar(50) NOT NULL,
+  `qtd` int(4) NOT NULL,
+  `status` smallint(6) DEFAULT NULL,
+  `datasolicitacao` date DEFAULT NULL,
+  `datareposicao` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_produto_reposicao` (`id_produto`),
+  CONSTRAINT `fk_produto_reposicao` FOREIGN KEY (`id_produto`) REFERENCES `produtos` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `usuarios`
---
-
-CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
+-- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela erp.usuarios
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(255) NOT NULL,
   `usuario` varchar(255) NOT NULL,
   `senha` varchar(255) NOT NULL,
@@ -158,167 +137,25 @@ CREATE TABLE `usuarios` (
   `permissao` int(11) NOT NULL,
   `status` int(11) NOT NULL,
   `criado` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `modificado` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `modificado` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
---
--- Extraindo dados da tabela `usuarios`
---
-
-INSERT INTO `usuarios` (`id`, `nome`, `usuario`, `senha`, `email`, `permissao`, `status`, `criado`, `modificado`) VALUES
-(1, 'teste', 'teste', 'ebd6d2f5d60ff9afaeda1a81fc53e2d0', 'teste@teste.com', 20, 1, '2017-08-23 07:10:00', NULL),
-(2, 'teste4', 'teste4', 'ebd6d2f5d60ff9afaeda1a81fc53e2d0', 'teste4@teste.com', 20, 1, '0000-00-00 00:00:00', NULL);
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `vendas`
---
-
-CREATE TABLE `vendas` (
-  `id` int(11) NOT NULL,
+-- Exportação de dados foi desmarcado.
+-- Copiando estrutura para tabela erp.vendas
+CREATE TABLE IF NOT EXISTS `vendas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_produto` int(11) NOT NULL,
   `qtd` int(11) NOT NULL,
   `status` int(11) NOT NULL DEFAULT '1',
   `datasolicitacao` date NOT NULL,
-  `dataaprovada` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `dataaprovada` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_produto_vendas` (`id_produto`),
+  CONSTRAINT `fk_produto_vendas` FOREIGN KEY (`id_produto`) REFERENCES `produtos` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
---
--- Extraindo dados da tabela `vendas`
---
-
-INSERT INTO `vendas` (`id`, `id_produto`, `qtd`, `status`, `datasolicitacao`, `dataaprovada`) VALUES
-(1, 1, 20, 0, '2017-09-04', NULL),
-(2, 1, 1, 2, '2017-09-04', '2017-09-04'),
-(3, 1, 2, 2, '2017-09-04', '2017-09-04'),
-(4, 1, 2, 2, '2017-09-04', '2017-09-04');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `compras`
---
-ALTER TABLE `compras`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_produto` (`id_produto`),
-  ADD KEY `fk_usuario` (`id_usuario`),
-  ADD KEY `fk_fornecedor` (`id_fornecedor`);
-
---
--- Indexes for table `fornecedores`
---
-ALTER TABLE `fornecedores`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `razaosocial` (`razaosocial`),
-  ADD UNIQUE KEY `fantasia` (`fantasia`),
-  ADD UNIQUE KEY `cnpj` (`cnpj`);
-
---
--- Indexes for table `historico`
---
-ALTER TABLE `historico`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_produto_historico` (`id_produto`);
-
---
--- Indexes for table `produtos`
---
-ALTER TABLE `produtos`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `reposicoes`
---
-ALTER TABLE `reposicoes`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_produto_reposicao` (`id_produto`);
-
---
--- Indexes for table `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `vendas`
---
-ALTER TABLE `vendas`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_produto_vendas` (`id_produto`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `compras`
---
-ALTER TABLE `compras`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT for table `fornecedores`
---
-ALTER TABLE `fornecedores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `historico`
---
-ALTER TABLE `historico`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `produtos`
---
-ALTER TABLE `produtos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `reposicoes`
---
-ALTER TABLE `reposicoes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `vendas`
---
-ALTER TABLE `vendas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
--- Constraints for dumped tables
---
-
---
--- Limitadores para a tabela `compras`
---
-ALTER TABLE `compras`
-  ADD CONSTRAINT `fk_fornecedor` FOREIGN KEY (`id_fornecedor`) REFERENCES `fornecedores` (`id`),
-  ADD CONSTRAINT `fk_produto` FOREIGN KEY (`id_produto`) REFERENCES `produtos` (`id`),
-  ADD CONSTRAINT `fk_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
-
---
--- Limitadores para a tabela `historico`
---
-ALTER TABLE `historico`
-  ADD CONSTRAINT `fk_produto_historico` FOREIGN KEY (`id_produto`) REFERENCES `produtos` (`id`);
-
---
--- Limitadores para a tabela `reposicoes`
---
-ALTER TABLE `reposicoes`
-  ADD CONSTRAINT `fk_produto_reposicao` FOREIGN KEY (`id_produto`) REFERENCES `produtos` (`id`);
-
---
--- Limitadores para a tabela `vendas`
---
-ALTER TABLE `vendas`
-  ADD CONSTRAINT `fk_produto_vendas` FOREIGN KEY (`id_produto`) REFERENCES `produtos` (`id`);
-COMMIT;
-
+-- Exportação de dados foi desmarcado.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
